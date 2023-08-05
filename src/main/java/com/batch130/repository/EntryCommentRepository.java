0605639e8b553b851c6cc91ef7388c62f34cba38
Fatih Deniz <deniz.fatih@hotmail.de>
2023-08-05 16:28:01 +0200

    EntryCommentController created (#8)
    Co-authored-by: SerkanCicek1 <serkann017@gmail.com>

---
+++ src/main/java/com/batch130/repository/EntryCommentRepository.java
@@ -0,0 +1,9 @@
+package com.batch130.repository;
+
+import com.batch130.entity.concretes.EntryComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntryCommentRepository extends JpaRepository<EntryComment, Integer> {
+
+
+}
